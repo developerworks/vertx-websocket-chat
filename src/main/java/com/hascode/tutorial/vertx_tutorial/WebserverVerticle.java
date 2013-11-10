09c73c16e8ab2404b0a63fa727821608e06f83f7
Micha Kops <micha.kops@gmail.com>
2013-11-10 19:08:35 +0100

    Recipient registration fixed.

--- src/main/java/com/hascode/tutorial/vertx_tutorial/WebserverVerticle.java
+++ src/main/java/com/hascode/tutorial/vertx_tutorial/WebserverVerticle.java
@@ -7,7 +7,6 @@ import java.util.Date;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.buffer.Buffer;
 import org.vertx.java.core.eventbus.EventBus;
-import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.core.http.RouteMatcher;
 import org.vertx.java.core.http.ServerWebSocket;
@@ -42,47 +41,44 @@ public class WebserverVerticle extends Verticle {
 		vertx.createHttpServer().websocketHandler(new Handler<ServerWebSocket>() {
 			@Override
 			public void handle(final ServerWebSocket ws) {
-				if (ws.path().startsWith("/chat")) {
-					ws.closeHandler(new Handler<Void>() {
-						@Override
-						public void handle(final Void event) {
-							String id = ws.textHandlerID();
-							String chatRoom = "arduino";
-							logger.info("un-registering connection with id: " + id + " from chat-room: " + chatRoom);
-							vertx.sharedData().getSet("chat.room.arduino").remove(id);
-						}
-					});
-
-					ws.dataHandler(new Handler<Buffer>() {
-						@Override
-						public void handle(final Buffer data) {
-							String id = ws.textHandlerID();
-							String chatRoom = "arduino";
-							logger.info("registering new connection with id: " + id + " for chat-room: " + chatRoom);
-							vertx.sharedData().getSet("chat.room" + chatRoom).add(id);
-
-							ObjectMapper m = new ObjectMapper();
-							try {
-								JsonNode rootNode = m.readTree(data.toString());
-								((ObjectNode) rootNode).put("received", new Date().toString());
-								String jsonOutput = m.writeValueAsString(rootNode);
-								logger.info("json generated: " + jsonOutput);
-								eventBus.send("chat.room.onmessage", jsonOutput);
-							} catch (IOException e) {
-								ws.reject();
-							}
-						}
-					});
-				} else {
+				if (!ws.path().matches("/chat/\\w+")) {
 					ws.reject();
+					return;
 				}
 
-				eventBus.registerHandler("chat.room.onmessage", new Handler<Message<String>>() {
+				final String chatRoom = "arduino";
+				final String id = ws.textHandlerID();
+				logger.info("registering new connection with id: " + id + " for chat-room: " + chatRoom);
+				vertx.sharedData().getSet("chat.room." + chatRoom).add(id);
+
+				ws.closeHandler(new Handler<Void>() {
 					@Override
-					public void handle(final Message<String> event) {
-						ws.writeTextFrame(event.body());
+					public void handle(final Void event) {
+						logger.info("un-registering connection with id: " + id + " from chat-room: " + chatRoom);
+						vertx.sharedData().getSet("chat.room." + chatRoom).remove(id);
 					}
 				});
+
+				ws.dataHandler(new Handler<Buffer>() {
+
+					@Override
+					public void handle(final Buffer data) {
+
+						ObjectMapper m = new ObjectMapper();
+						try {
+							JsonNode rootNode = m.readTree(data.toString());
+							((ObjectNode) rootNode).put("received", new Date().toString());
+							String jsonOutput = m.writeValueAsString(rootNode);
+							logger.info("json generated: " + jsonOutput);
+							for (Object chatter : vertx.sharedData().getSet("chat.room." + chatRoom)) {
+								eventBus.send((String) chatter, jsonOutput);
+							}
+						} catch (IOException e) {
+							ws.reject();
+						}
+					}
+				});
+
 			}
 		}).listen(8090);
 	}
